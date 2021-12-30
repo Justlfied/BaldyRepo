c806354fe6cdda8fa59730247435ce09e2f38ef7
Justlfied <57233854+Justlfied@users.noreply.github.com>
2021-12-30 17:49:26 +0100

    Create OverloadTimerBrewConfig.java

---
+++ Overload/OverloadTimerBrewConfig.java
@@ -0,0 +1,38 @@
+package net.runelite.client.plugins.overloadtimerbrew;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+@ConfigGroup("overloadtimerbrew")
+public interface OverloadTimerBrewConfig extends Config {
+    @ConfigItem(
+        position = 1,
+        keyName = "tickCounter",
+        name = "Tick Counter",
+        description = "Shows ticks instead of semi real time"
+    )
+    default boolean tickCounter() {
+        return false;
+    };
+
+    @ConfigItem(
+        position = 2,
+        keyName = "tickOverlay",
+        name = "Tickcounter Overlay",
+        description = "Shows very aggresive looking overlay of ticks until brew"
+    )
+    default boolean tickOverlay() {
+        return false;
+    }
+
+    @ConfigItem(
+        position = 3,
+        keyName = "cycleCounter",
+        name = "Show remaining cycles",
+        description = "Change the tickcounter to a cycle counter"
+    )
+    default boolean cycleCounter() {
+        return true;
+    }
+}
